53cfd19149c62c65f75adfd79686dc43e82e4534
David Guijarro <guijarro.dav@gmail.com>
2020-06-17 16:51:04 +0200

    docs: build and deploy docs in the pipeline

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,7 @@
 const apiSideBar = require('./api-sidebar.json');
 
 module.exports = {
+  base: '/tsdash/', // for deploying the docs to GH Pages
   title: 'tsdash',
   description: 'A modern utility library for JavaScript and TypeScript.',
   themeConfig: {
