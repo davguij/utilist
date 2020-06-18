5aaed17b3a92c4069a275f43bba73f904b3d9a17
David Guijarro <guijarro.dav@gmail.com>
2020-06-18 13:23:32 +0200

    chore: rename project

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,8 @@
 const apiSideBar = require('./api-sidebar.json');
 
 module.exports = {
-  base: '/tsdash/', // for deploying the docs to GH Pages
-  title: 'tsdash',
+  base: '/utilist/', // for deploying the docs to GH Pages
+  title: 'utilist',
   description: 'A modern utility library for JavaScript and TypeScript.',
   themeConfig: {
     sidebar: [...apiSideBar],
